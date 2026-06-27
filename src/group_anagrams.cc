aa499029d5c85284a1eef125247b54e309f5e5ad
jxge <jxge@yahoo.com>
2026-06-27 15:25:58 -0700

    Refactor groupAnagrams with multiple solution classes

--- src/group_anagrams.cc
+++ src/group_anagrams.cc
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         // Hash map to store sorted string signature -> list of original strings
@@ -23,7 +23,7 @@ public:
 
 // Input: strs = ["act","pots","tops","cat","stop","hat"]
 // map:   { a#1c#1t1 -> act, a#1c#1#t#1 -> cat
-class Solution {
+class Solution2 {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> anagramGroups;
@@ -60,3 +60,45 @@ public:
     }
 };
 
+using namespace std;
+
+// 🚀 Magic Ingredient: Custom hash function for std::array so it can be used as an unordered_map key
+struct ArrayHash {
+    size_t operator()(const array<int, 26>& arr) const {
+        size_t hashValue = 0;
+        for (int count : arr) {
+            // A simple combined hash formula (similar to boost::hash_combine)
+            hashValue ^= hash_val(count) + 0x9e3779b9 + (hashValue << 6) + (hashValue >> 2);
+        }
+        return hashValue;
+    }
+private:
+    hash<int> hash_val;
+};
+
+class Solution3 {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // ⚡ Map directly from the 26-count array to the vector of strings
+        unordered_map<array<int, 26>, vector<string>, ArrayHash> anagramGroups;
+        anagramGroups.reserve(strs.size()); // Pre-allocate map buckets
+        
+        for (const string& s : strs) {
+            array<int, 26> count = {0}; // Fixed-size stack array
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+            
+            // Zero string serialization overhead! Direct map insertion.
+            anagramGroups[count].push_back(s);
+        }
+        
+        vector<vector<string>> result;
+        result.reserve(anagramGroups.size());
+        for (auto& pair : anagramGroups) {
+            result.push_back(move(pair.second));
+        }
+        
+        return result;
+    }
+};
