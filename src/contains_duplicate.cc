5e270a42203c0323153c7bf0be3cd4aaea5e89eb
jxge <jxge@yahoo.com>
2026-06-27 14:59:58 -0700

    Implement alternative containsDuplicate solution
    Added a second solution for checking duplicates using insert method.

--- src/contains_duplicate.cc
+++ src/contains_duplicate.cc
@@ -1,16 +1,34 @@
 #include <vector>
 #include <unordered_set>
 
-class Solution {
+class Solution1 {
 public:
     bool containsDuplicate(std::vector<int>& nums) {
         std::unordered_set<int> seen;
+        seen.reserve(nums.size());  // worse case (all number are different) you need nums.size
         
         for (int num : nums) {
             if (seen.count(num)) {
                 return true;
             }
             seen.insert(num);
+        }
+        
+        return false;
+    }
+};
+
+class Solution2 {
+public:
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> seen;
+        seen.reserve(nums.size());  // worse case (all number are different) you need nums.size
+        
+        for (int num : nums) {
+            auto [_, created] = seen.insert(num);
+            if (!created) {
+                return true;
+            }
         }
         
         return false;
